c98c3d7febe07dd4a3b4d2d7f3fee3a92cd3b70c
bootcd <ultra.bootcd@gmail.com>
2018-06-25 15:28:01 +0300

    Update lowcpuprior.sh

--- lowcpuprior.sh
+++ lowcpuprior.sh
@@ -4,6 +4,6 @@ i=1
 while [ $i=1 ]
 do
 echo " [`date`] START LOW proirity copy" >> /var/log/cpuprior.log
-cp -f /etc/httpd/conf/httpd.conf /root
+cp -f /etc/httpd/conf/httpd.conf /home/vagrant
 echo " [`date`] STOP LOW priority copy" >> /var/log/cpuprior.log
 done
