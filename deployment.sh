51c790cac6911f69c8207c2c513345ce1a8f89f4
bootcd <ultra.bootcd@gmail.com>
2018-06-25 15:53:55 +0300

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 yum -y install httpd
-touch /var/log/cpuprior.log
+sudo touch /var/log/cpuprior.log
 sudo mkdir /concurents
 cd /vagrant
 cp cpuconcurent.target hiprior.service lowprior.service /etc/systemd/system
